5f2c3e021c0b59770ae55b96abfd20964eee7263
Antti Kivi <antti.kivi@visiosto.fi>
2021-10-28 21:14:52 +0300

    build: add Rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2021 Visiosto oy
+// Licensed under the MIT License
+
+// Modified from file 'rollup.config.js' in primer/react repository.
+// The original code is available at
+// https://github.com/primer/react/blob/main/rollup.config.js
+
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+const formats = ['esm', 'umd']
+
+const plugins = [
+  babel({extensions, exclude: 'node_modules/**', runtimeHelpers: true}),
+  resolve({extensions}),
+  commonjs(),
+]
+
+export default [
+  {
+    input: 'src/index.ts',
+    external: ['styled-components', 'react', 'react-dom'],
+    plugins,
+    output: formats.map(format => ({
+      file: `dist/browser.${format}.js`,
+      format,
+      sourcemap: true,
+      name: 'primer',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'styled-components': 'styled'
+      }
+    }))
+  }
+]
