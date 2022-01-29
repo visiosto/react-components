57bd7da5292bda6155b83197f801b7923b0c4171
Antti Kivi <antti.kivi@visiosto.fi>
2022-01-29 18:11:53 +0200

    feat: add class name to the external link

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com), and this 
 
 ## [Unreleased]
 
+### Added
+
+- `className` property to the external link component to make it function with styled-components.
+
 ### Removed
 
 - Cookie settings class from the global style as the library doesn’t have the cookie component.
--- src/ExternalLink.tsx
+++ src/ExternalLink.tsx
@@ -7,12 +7,13 @@ import Icon from './Icon';
 
 export type ExternalLinkProps = {
   children: React.ReactNode;
+  className?: string;
   to: string;
 };
 
-function ExternalLink({ children, to }: ExternalLinkProps) {
+function ExternalLink({ children, className = undefined, to }: ExternalLinkProps) {
   return (
-    <a href={to} rel="noopener noreferrer" target="_blank">
+    <a className={className} href={to} rel="noopener noreferrer" target="_blank">
       {children} <Icon icon="open_in_new" size={18} />
     </a>
   );
